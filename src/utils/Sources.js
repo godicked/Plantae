97d9bab36114f0f22cef4f20f80a977c0f7d14f5
david <godicked@gmail.com>
2020-04-05 16:29:55 +0200

    better date fusion.

--- src/utils/Sources.js
+++ src/utils/Sources.js
@@ -15,12 +15,25 @@ export let computePlant = function(plant) {
 }
 
 // aggregate multiples dates to one weighted dates array
-export let computeDates = function(sources)  {
+export let computeDates = function(sources, override = true)  {
     // console.log('compute: ' + sources.length + ' cmp: ' + sources[0].computed)
     let dates = []
     let weight = []
     for(let i = 0; i < 24; i++) {
-        let max = Math.max(...sources.map(s => s.dates[i]))
+
+        let max = 0
+
+        if(override) {
+            let c1 = sources.filter(s => s.dates[i] === 1).length
+            let c2 = sources.filter(s => s.dates[i] === 2).length
+
+            if(c1 > 0 && c1 > c2) {max = 1}
+            if(c2 > 0 && c2 >= c1) {max = 2} // at equal occurence option 2 is still stronger than 1
+        }
+        else {
+            max = Math.max(...sources.map(s => s.dates[i]))
+        }
+
         dates.push(max)
 
         let w = 0
